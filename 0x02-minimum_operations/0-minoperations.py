eb72da7b40d2467ad0065f28017df99cae320a7e
Ikechukwu-Miracle <nwekeiyke46@gmail.com>
2024-03-22 06:07:57 +0100

    created 0-minoperations.py

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Minimum operations required to print a certain number of characters."""
+
+
+def minOperations(n):
+    """Returns the required minimum operations"""
+    if n <= 1:
+        return 0
+
+    for i in range (2, n + 1):
+        if n % i == 0:
+            return minOperations(n // i) + i
+    return n
+
