de65a294d39d55307f90a954b20a074c4ab6fb25
Ikechukwu-Miracle <nwekeiyke46@gmail.com>
2024-05-10 04:29:20 +0100

    0-island_perimeter.py

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -7,7 +7,7 @@ def island_perimeter(grid):
     as an NxN matrix.
     Args:
         grid (list of list): The grid where the Island exist in
-        
+
     Retuns:
         The total perimeter of the Island
     """
@@ -15,19 +15,18 @@ def island_perimeter(grid):
     column = len(grid[0])
     perimeter = 0
     connections = 0
-    
+
     if row > 100 or column > 100:
         return 0
-    
+
     for x in range(row):
         for y in range(column):
             if grid[x][y] == 1:
                 perimeter += 4
-                
+
                 if x != 0 and grid[x - 1][y] == 1:
                     connections += 1
                 if y != 0 and grid[x][y - 1] == 1:
                     connections += 1
-                    
+
     return perimeter - (connections * 2)
-    
